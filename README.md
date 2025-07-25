68675ac74fdf8072f7b08dc5f37473e9c0ffe24a
mzulfian <mzulfianbisnis@gmail.com>
2025-07-25 14:24:37 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+## Deskripsi
+
+Situs web pernikahan adalah tempat berbagi informasi tentang acara pernikahan kepada keluarga, teman, dan tamu undangan. Semoga situs web ini membantu dalam menyebarkan informasi penting, seperti lokasi acara, jadwal, dan lainnya.
+
+## Penghargaan Desain
+
+Proyek ini merupakan redesain dari undangan pernikahan milik [Dewanakl](https://github.com/dewanakl) dan [sdprdh](https://github.com/sdprdh)
+
+Terima kasih telah mengunjungi repositori ini. Semoga hari pernikahan Anda berjalan lancar dan penuh kebahagiaan!
---
+++ src/assets/data/data.js
@@ -0,0 +1,126 @@
+export const data = {
+    bride: {
+        L: {
+            id: 1,
+            name: 'Lailatul Kadri',
+            child: 'Putri ke dari',
+            father: 'Bapak Azir',
+            mother: 'Ibu Zuriati',
+            image: './src/assets/images/cowo.png'
+        },
+        P: {
+            id: 2,
+            name: 'Rahmat Ilahi',
+            child: 'Putra dari',
+            father: 'Bapak Syahrul',
+            mother: 'Ibu Ernida',
+            image: './src/assets/images/cewe.png'
+        },
+
+        couple: './src/assets/images/couple.png'
+    },
+
+    time: {
+        marriage: {
+            year: '2025',
+            month: 'September',
+            date: '15',
+            day: 'Senin',
+            hours: {
+                start: '08.00',
+                finish: 'Selesai'
+            }
+        },
+        reception: {
+            year: '2025',
+            month: 'September',
+            date: '15',
+            day: 'Senin',
+            hours: {
+                start: '10.00',
+                finish: 'Selesai'
+            }
+        },
+        address: 'Korong Tanjuang, Nagari Gasan Gadang (Samping Kantor Camat Batang Gasan)'
+    },
+
+    link: {
+        calendar: 'https://calendar.app.google/k4zaKypRuUyT7wFc6',
+        map: 'https://maps.app.goo.gl/EmXhA9doedkb1nSM6',
+    },
+
+    galeri: [
+        {
+            id: 1,
+            image: './src/assets/images/1.png'
+        },
+        {
+            id: 2,
+            image: './src/assets/images/2.png'
+        },
+        {
+            id: 3,
+            image: './src/assets/images/3.png'
+        },
+        {
+            id: 4,
+            image: './src/assets/images/4.png'
+        },
+        {
+            id: 5,
+            image: './src/assets/images/5.png'
+        }
+    ],
+
+    bank: [
+        {
+            id: 1,
+            name: 'Lorem Ipsum',
+            icon: './src/assets/images/bca.png',
+            rekening: '12345678'
+        },
+        {
+            id: 2,
+            name: 'Ipsum Lorem',
+            icon: './src/assets/images/bri.png',
+            rekening: '12345678'
+        },
+    ],
+
+    audio: './src/assets/audio/wedding.mp3',
+
+    api: 'https://script.google.com/macros/s/AKfycbxuj_508A5OP3tyA70z80iTLoEEBX7hdNK7LqiHHIgalTyV0L_Ic35JGAPtIVbEebQ/exec',
+
+    navbar: [
+        {
+            id: 1,
+            teks: 'Home',
+            icon: 'bx bxs-home-heart',
+            path: '#home',
+        },
+        {
+            id: 2,
+            teks: 'Mempelai',
+            icon: 'bx bxs-group',
+            path: '#bride',
+        },
+        {
+            id: 3,
+            teks: 'Tanggal',
+            icon: 'bx bxs-calendar-check',
+            path: '#time',
+        },
+        {
+            id: 4,
+            teks: 'Galeri',
+            icon: 'bx bxs-photo-album',
+            path: '#galeri',
+        },
+        {
+            id: 5,
+            teks: 'Ucapan',
+            icon: 'bx bxs-message-rounded-dots',
+            path: '#wishas',
+        },
+    ],
+}
